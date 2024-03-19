b2866accb01854b885a0768ed662c7a694803e56
unknown <baycaohon1@gmail.com>
2024-03-19 17:16:03 +0700

    Done UI LoginSreen

--- app/src/main/java/com/example/loyaltyrewardapp/screens/LoginScreen.kt
+++ app/src/main/java/com/example/loyaltyrewardapp/screens/LoginScreen.kt
@@ -1,26 +1,40 @@
 package com.example.loyaltyrewardapp.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Facebook
+import androidx.compose.material.icons.rounded.VisibilityOff
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -28,15 +42,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.loyaltyrewardapp.R
 
 class Login : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -72,7 +90,7 @@ fun Tilte() {
 
         Text(
             text = "Đăng nhập",
-            style = androidx.compose.ui.text.TextStyle(fontSize = 24.sp),
+            style = androidx.compose.ui.text.TextStyle(fontSize = 32.sp),
             modifier = Modifier.padding(bottom = 10.dp),
             fontWeight = FontWeight.Bold
         )
@@ -80,7 +98,7 @@ fun Tilte() {
             text = "Hệ thống tích điểm tiện ích",
             modifier = Modifier.align(Alignment.BottomStart),
             style = androidx.compose.ui.text.TextStyle(
-                fontSize = 14.sp,
+                fontSize = 16.sp,
                 color = Color.Black.copy(alpha = 0.5f)
             )
         )
@@ -92,8 +110,8 @@ fun getField() {
     Column(
         modifier = Modifier
             .width(327.dp)
-            .height(300.dp)
-            .offset(x = 27.dp, y = 171.dp)
+            .fillMaxHeight()
+            .offset(x = 27.dp, y = 121.dp)
     ) {
         var numberPhone by remember {
             mutableStateOf("")
@@ -103,7 +121,7 @@ fun getField() {
             style = androidx.compose.ui.text.TextStyle(fontSize = 16.sp),
             color = Color.Black.copy(alpha = 0.5f)
         )
-        TextField(
+        OutlinedTextField(
             label = { Text("123456") },
             value = numberPhone,
             onValueChange = { numberPhone = it },
@@ -113,11 +131,15 @@ fun getField() {
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 20.dp)
-                .background(Color.White)
-                .clip(RoundedCornerShape(10.dp))
+                .background(Color.Transparent), shape = RoundedCornerShape(10.dp)
+
         )
         var password by remember {
             mutableStateOf("")
+
+        }
+        var isPasswordVisible by remember {
+            mutableStateOf(false)
         }
         Text(
             text = "Mật khẩu",
@@ -126,17 +148,26 @@ fun getField() {
             color = Color.Black
                 .copy(alpha = 0.5f)
         )
-        TextField(
+        OutlinedTextField(
             value = password,
             onValueChange = { password = it },
-            label = { Text("*******") },
-            visualTransformation = PasswordVisualTransformation(),
+            label = { Text("1237492") },
+            visualTransformation = if (isPasswordVisible)
+                VisualTransformation.None
+            else PasswordVisualTransformation(),
+            trailingIcon = {
+                IconButton(
+                    onClick = {
+                        isPasswordVisible = !isPasswordVisible
+                    }
+                ) {
+                    Icon(Icons.Rounded.VisibilityOff, contentDescription = "Show password")
+                }
+            },
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 20.dp)
-                .clip(RoundedCornerShape(10.dp)),
-
-            )
+                .padding(top = 20.dp), shape = RoundedCornerShape(10.dp)
+        )
         Button(
             onClick = { /*TODO*/ },
             modifier = Modifier
@@ -155,9 +186,101 @@ fun getField() {
                 modifier = Modifier.padding(top = 3.dp, bottom = 3.dp)
             )
         }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 30.dp)
+        ) {
+            Spacer(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(1.dp)
+                    .alpha(0.1f)
+                    .background(color = Color.Black)
+                    .align(Alignment.CenterVertically)
+            )
+            Text(
+                text = "Hoặc đăng nhập",
+                color = Color.Black.copy(alpha = 0.5f),
+                modifier = Modifier.padding(start = 5.dp, end = 5.dp)
+            )
+            Spacer(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(1.dp)
+                    .alpha(0.1f)
+                    .background(color = Color.Black)
+                    .align(Alignment.CenterVertically)
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 30.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Button(
+                onClick = {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse("https://www.google.com")
+                    /*TODO*/
+                },
+                modifier = Modifier
+                    .alpha(0.9f)
+                    .clip(RoundedCornerShape(10.dp)),
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFF5F5F5))
+            ) {
+                Image(painter = painterResource(id = R.drawable.google), contentDescription = "Icon Google", modifier = Modifier.size(size = 18.dp))
+                Text(
+                    text = "Google", fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(start = 5.dp, top = 10.dp, end = 30.dp, bottom = 10.dp)
+                        .align(Alignment.CenterVertically),
+                    fontSize = 14.sp,
+                )
+            }
+            Button(
+                onClick = {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse("https://www.facebook.com")/*TODO*/
+                },
+                modifier = Modifier
+                    .alpha(0.9f)
+                    .clip(RoundedCornerShape(10.dp)),
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFF5F5F5))
+            ) {
+                Icon((Icons.Filled.Facebook), contentDescription = "", tint = Color.Blue)
+                Text(
+                    text = "Facebook", fontWeight = FontWeight.Bold, modifier = Modifier
+                        .padding(start = 5.dp, top = 10.dp, end = 25.dp, bottom = 10.dp)
+                        .align(Alignment.CenterVertically), fontSize = 14.sp
+                )
+            }
+
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 30.dp),
+            Arrangement.Center
+        ) {
+            Text(text = "Chưa có tài khoản ?", modifier = Modifier.alpha(0.5f))
+            Text(
+                text = "Đăng ký", modifier = Modifier
+                    .clickable(onClick = {
+                        Unit
+                    })
+                    .padding(start = 3.dp), color = Color(0xFF37A1ED)
+            )
+
+        }
+
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
