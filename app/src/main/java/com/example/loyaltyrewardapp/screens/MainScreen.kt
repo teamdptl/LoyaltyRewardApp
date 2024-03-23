7e14f16065fffb2f20cc5024c5e5605c9aba3a9a
trangthanhphuc <trangthanhphuc91@gmail.com>
2024-03-23 18:54:32 +0700

    update navigation

--- app/src/main/java/com/example/loyaltyrewardapp/screens/MainScreen.kt
+++ app/src/main/java/com/example/loyaltyrewardapp/screens/MainScreen.kt
@@ -1,10 +1,18 @@
 package com.example.loyaltyrewardapp.screens
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.BottomNavigation
+import com.example.loyaltyrewardapp.navigation.AppNavigation
 
 class MainScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            BottomNavigation {
+                AppNavigation()
+            }
+        }
     }
 }
