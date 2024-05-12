f0f3e6d2568e840891e0222a4bd811318d5a05f6
trangthanhphuc <trangthanhphuc91@gmail.com>
2024-05-13 00:49:35 +0700

    update splashScreen

--- app/src/main/java/com/example/loyaltyrewardapp/screens/SplashScreen.kt
+++ app/src/main/java/com/example/loyaltyrewardapp/screens/SplashScreen.kt
@@ -1,10 +1,39 @@
 package com.example.loyaltyrewardapp.screens
 
-import android.window.SplashScreen
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.loyaltyrewardapp.R
 
+@Preview
 @Composable
-fun SplashScreen(){
-    Text("Splash Screen")
+fun SplashScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Image(
+            painterResource(id = R.drawable.avatar),
+            contentDescription = "Logo",
+            modifier = Modifier
+                .width(200.dp) // Width of the logo
+                .height(200.dp), // Height of the logo
+            contentScale = ContentScale.Fit // Scale type of the image
+        )
+    }
 }
